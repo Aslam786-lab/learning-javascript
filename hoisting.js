92d6ddb4c4bda3409c42e80ba2dc053827d34188
Aslam786-lab <aslamkanker786@gmail.com>
2022-08-06 17:57:25 +0530

    second commit

--- hoisting.js
+++ hoisting.js
@@ -1,11 +1,42 @@
-console.log(a);
-console.log(getName());
-console.log(getName);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// //normal funtion
+// console.log(getName());
+// console.log(getName);
+
+//function expression
+// console.log(fun());
+
+// //arrow function
+// console.log(arrow());
 
 //hoisting in javascript
+var a=15;
+let b=20;     //hoisting of let is not possible
+const c= 30;  //hoisting of const is not possible
+
+//functions 
+//normal funtion(possible)
 function getName() {
     console.log("hello javascript");
 }
 
-var a=15;
+//function expression(not possible for function but variable is hoisted with undefined)
+var fun = function(){
+    console.log("hello javascript");
+}
+
+//arrow function() (not possible for function but variable is hoisted with undefined)
+var arrow = () => "hello javascript";
+
+//IIFE 
+//hoisting of IIFE is not possible because it is a expression not statment
+(function(){
+    console.log("hello javascript");
+}());
+
+
+
 
