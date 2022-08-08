72f9a19e111b45fcf8f7d917a3ca2c9ab57528fe
Aslam786-lab <aslamkanker786@gmail.com>
2022-08-08 14:36:56 +0530

    second commit

--- calculator for different data types/calculator.js
+++ calculator for different data types/calculator.js
@@ -4,19 +4,22 @@ function getInputType() {
   const sinput = document.getElementById('input2').value;
   const reg=/^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$/;
   const spaceReg=/^ +$/;
+  document.getElementById("result").innerHTML = "Output";
 
 
 
-  if(finput === "" || sinput === "" || spaceReg.test(finput)=== true || spaceReg.test(sinput) === true){
-    document.getElementById("result").innerHTML = "Result";
+  if(finput === "" || sinput === "" ){
     document.getElementById("inputResult").innerHTML = "Please Enter the input ";
 
 
-  }else if(finput === 'true' || finput === 'false' || sinput === 'true' || sinput === 'false'){
+  }else if(spaceReg.test(finput)=== true || spaceReg.test(sinput) === true){
+    document.getElementById("inputResult").innerHTML = "Blank space is not allowed as input";
+  }
+  else if(finput === 'true' || finput === 'false' || sinput === 'true' || sinput === 'false'){
 
     return calculateBoolean(finput,sinput);
 
-  }else if(reg.test(finput) == true ||  reg.test(sinput) == true ){
+  }else if(reg.test(finput) == true &&  reg.test(sinput) == true ){
     const op = prompt('Enter the operator ( either +, -, * or / ):');
 
     return calculateNumber(parseFloat(finput),parseFloat(sinput),op);
@@ -29,28 +32,25 @@ function getInputType() {
 //==============================================================================================================================================
 
 function calculateNumber(num1,num2,op) {
+  document.getElementById("result").innerHTML = "Output";
   switch (op) {
     case '+':
       const addRes = num1 + num2;
-      document.getElementById("result").innerHTML = "Result";
       document.getElementById("inputResult").innerHTML = addRes;
       break;
 
     case '-':
       const subRes = num1 - num2;
-      document.getElementById("result").innerHTML = "Result";
       document.getElementById("inputResult").innerHTML = subRes;
       break;
 
     case '*':
       const mulRes = num1 * num2;
-      document.getElementById("result").innerHTML = "Result";
       document.getElementById("inputResult").innerHTML = mulRes;
       break;
 
     case '/':
       const divRes = num1 / num2;
-      document.getElementById("result").innerHTML = "Result";
       document.getElementById("inputResult").innerHTML = divRes;
       break;
 
@@ -61,32 +61,29 @@ function calculateNumber(num1,num2,op) {
 
 function calculateString(str1,str2) {
   const strRes = str1 + str2;
-  document.getElementById("result").innerHTML = "Result";
+  document.getElementById("result").innerHTML = "Output";
   document.getElementById("inputResult").innerHTML = strRes;
 
 }
 
 function calculateBoolean(bool1,bool2) {
 
-
+  document.getElementById("result").innerHTML = "Output";
 
   if(bool1 == "true" && bool2 == "true"){
-    document.getElementById("result").innerHTML = "Result";
     document.getElementById("inputResult").innerHTML = "false";
 
   }else if(bool1 == "false" && bool2 == "false"){
-    document.getElementById("result").innerHTML = "Result";
     document.getElementById("inputResult").innerHTML = "false";
 
   }else if(bool1 == "true" && bool2 == "false"){
-
-    document.getElementById("result").innerHTML = "Result";
     document.getElementById("inputResult").innerHTML = "true";
 
   }else if(bool1 == "false" && bool2 == "true"){
-    document.getElementById("result").innerHTML = "Result";
     document.getElementById("inputResult").innerHTML = "true";
 
+  }else{
+    document.getElementById("inputResult").innerHTML = "Invalid input";
   }
 
 
