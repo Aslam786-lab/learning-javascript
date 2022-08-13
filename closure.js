219c811e584410c5886d3922b4c9e6f8efab8a8c
Aslam786-lab <aslamkanker786@gmail.com>
2022-08-13 11:56:30 +0530

    second commit

--- closure.js
+++ closure.js
@@ -14,3 +14,24 @@ let z=name()
 console.log(z())
 
 
+
+//======================================
+function counter() {
+    let count=0;
+
+    return function increment() {
+        count++;
+        console.log(count);
+    }
+}
+
+let var1=counter();
+var1()  //1
+var1()  //2
+var1()  //3
+
+let var2 =counter();
+var2()  //1
+
+
+
