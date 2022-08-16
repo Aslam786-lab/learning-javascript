11c72dc34aba0dac03845f5f45a54116b4db294f
Aslam786-lab <aslamkanker786@gmail.com>
2022-08-16 18:55:08 +0530

    second commit

--- prototype-array-methods.js
+++ prototype-array-methods.js
@@ -31,13 +31,13 @@ Array.prototype.myReduce=function(fn,val){
 
 const arr=[100,300,500,200,1000]
 const arr2=[
-    { name: "bike",     price: 1000},
-    { name: "pc",       price: 500},
-    { name: "book",     price: 60},
-    { name: "album",    price: 400},
-    { name: "phone",    price: 600},
-    { name: "keyboard", price: 40},
-    { name: "tv",       price: 80}
+    { name: "bike",     price: 1000, year: 2019},
+    { name: "pc",       price: 500, year: 2016},
+    { name: "book",     price: 60, year: 2019},
+    { name: "album",    price: 400, year: 2008},
+    { name: "phone",    price: 600, year: 2007},
+    { name: "keyboard", price: 40, year: 2008},
+    { name: "tv",       price: 80, year: 2019}
 ]
 
 //map
@@ -50,4 +50,19 @@ const resF = arr.myFilter((x) => x>500)
 
 //reduce
 const resR = arr2.myReduce((sum,item) => {return sum+item.price},0)
-console.log(resR)
+// console.log(resR)
+
+
+
+//problem list out the perticular years with number of items brought ex{2019: 3, 2008:2,2007: 1....}
+
+const yearR = arr2.myReduce((obj, item) => {
+    if(obj[item.year]){
+        obj[item.year]++
+    }else{
+        obj[item.year]=1
+    }
+    return obj;
+},{})
+
+console.log(yearR)     //{ '2007': 1, '2008': 2, '2016': 1, '2019': 3 }
