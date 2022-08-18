9e7f5bdcefc830e7fc16b3511ad8ed5fecd8feea
Aslam786-lab <aslamkanker786@gmail.com>
2022-08-18 13:25:43 +0530

    second commit

--- exception-handling.js
+++ exception-handling.js
@@ -1,14 +1,32 @@
-try{
-    console.log("before the error")
-    invlid;
-    console.log("after the error");
-}catch(error){
-    // console.log(error);
-    console.log(error.name);
-    console.log(error.message);
+// try{
+//     console.log("before the error")
+//     invlid;
+//     console.log("after the error");
+// }catch(error){
+//     // console.log(error);
+//     console.log(error.name);
+//     console.log(error.message);
     
-}finally{
-    console.log("finally will execute regrdless of error")
-}
+// }finally{
+//     console.log("finally will execute regrdless of error")
+// }
 
-console.log("Error is handled");
+// console.log("Error is handled");
+
+
+//==========================================================
+//throw
+let json = '{"age": 50}';
+
+try {
+    let user =JSON.parse(json)
+
+    if(!user.name){
+        throw new SyntaxError("No name in user")
+    }
+    console.log(user.name);
+
+} catch (error) {
+    console.log(error.name);    //SyntaxError
+    console.log(error.message); //No name in user
+}
