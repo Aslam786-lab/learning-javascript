50d27b0cdd4a9d62ddd8b7c5c20424b5fc920112
Aslam786-lab <aslamkanker786@gmail.com>
2022-09-03 00:48:52 +0530

    first Element

--- to-do-list/src/to-do.js
+++ to-do-list/src/to-do.js
@@ -1,8 +1,8 @@
 // const frontUrl="http://127.0.0.1:5500/to-do-list/index.html"
-const getUrl ="http://localhost:3000/todo?_sort=id&_order=desc"
+const getUrl ="http://localhost:3000/todo"
 const putUrl ="http://localhost:3000/todo"
 const addButton =document.querySelector('.button')
-const deleteButton = document.querySelector('ul')
+const ulTag = document.querySelector('ul')
 
 
 //fetching from server
@@ -14,19 +14,6 @@ const getServerList=function(){
     })
 }
 getServerList().then(todos => getText(todos))
-
-//displaying promise object one by one in dom
-function getText(todo){
-   
-        todo.forEach(element => {
-            const li = document.createElement('li')
-            li.setAttribute("contenteditable", "true")
-            li.setAttribute('spellcheck', false)
-            li.innerHTML=element.text+`  <button class='button delete'>delete <p hidden>${element.id}</p></button>`
-            document.querySelector('ul').append(li)
-        })
-        
-}
 
 
 //adding text to the server
@@ -42,27 +29,80 @@ function addText(text){
     })
 
 }
+// ====================================================
 
-//passing text to the server and displaying on dom    
-addButton.addEventListener('click',function(){
-   const text =document.querySelector('.text').value
+
+
+//displaying promise object one by one in dom
+
+
+
+function getText(todo){
+   let arr=[]
+    todo.forEach((element,i) => {
+        const li = document.createElement('li')
+        li.setAttribute("contenteditable", "true")
+        li.setAttribute('spellcheck', false)
+        li.innerHTML=`<span data-element-id=${element.id}>${element.text}</span>`
+        // +`<p hidden>${element.id}</p>`
+        +`<div class="bothButton"><button class="button done" data-id=${i}>done</button>`
+        +`  <button class='button delete'>delete</button></div>`
+        
+        arr.push(li)
+        // document.querySelector('ul').append(li)
+    })
+    arr.reverse().forEach((item,i,arr)=>{
+        ulTag.append(arr[i])
+    })
+}
+
+
+//passing text to the server and displaying on dom   
+function displayDom(){
+    const text =document.querySelector('.text').value
    if(text === ''){
     return
    }
    addText(text).then(response => {
     getText(response)
    })
+}
+
+
+addButton.addEventListener('click',function(){
+   displayDom()
+})
+
+document.addEventListener('keyup',(e)=>{
+    if(e.key === 'Enter'){
+        displayDom()
+    }
 })
 
 
 
 
+// ========================================================
+//strike the todo
+ulTag.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('done')){
+       const listId = e.target.dataset.id
+       const id = ulTag.childNodes.length-1-listId
+       let liText= ulTag.childNodes[id].childNodes[0];
+       liText.innerHTML=`<del>${liText.textContent}</del>`
+        
+    }
+})
 
+
+// =======================================================
 //delete from the server
 
-deleteButton.addEventListener('click',function(e){
+ulTag.addEventListener('click',function(e){
     if(e.target.classList.contains('delete')){
-       const id= e.target.childNodes[1].textContent
+       const id= e.target.parentElement.parentElement.childNodes[0].dataset.elementId
+       
+       
        fetch(`${putUrl}/${id}`,{
         method:'DELETE'
        })
